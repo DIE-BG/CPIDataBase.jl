ad78058ffcc33f62d1bd4113c14e9f6fba2e597e
Documenter.jl <documenter@juliadocs.github.io>
2022-01-27 22:45:07 +0000

    build based on a3ffc9b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CPIDataBase","category":"page"},{"location":"#CPIDataBase","page":"Home","title":"CPIDataBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CPIDataBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CPIDataBase]","category":"page"}]
+}
